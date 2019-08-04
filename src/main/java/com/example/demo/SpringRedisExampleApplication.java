8b71d3c909753d706b8c6285f473a06038820514
Jeremy Staunton <jeremy.staunton@mycit.ie>
2019-08-04 17:05:55 +0100

    Jedis style added

--- src/main/java/com/example/demo/SpringRedisExampleApplication.java
+++ src/main/java/com/example/demo/SpringRedisExampleApplication.java
@@ -3,25 +3,27 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import com.example.demo.model.User;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example")
 public class SpringRedisExampleApplication {
 	
-	@Bean
-	JedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory();
-	}
-
-	@Bean
-	RedisTemplate<String, User> redisTemplate() {
-		RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(jedisConnectionFactory());
-		return redisTemplate;
- 	}
+//	@Bean
+//	JedisConnectionFactory jedisConnectionFactory() {
+//		return new JedisConnectionFactory();
+//	}
+//
+//	@Bean
+//	RedisTemplate<String, User> redisTemplate() {
+//		RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
+//		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+//		return redisTemplate;
+// 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRedisExampleApplication.class, args);
 	}
